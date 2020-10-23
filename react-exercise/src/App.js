7d460e4d19710e6cbb7d234c12181e51d1905f43
Barnabás Mátrai <matrai.barnabas1@gmail.com>
2020-10-23 22:22:17 +0200

    add background to login page

--- react-exercise/src/App.js
+++ react-exercise/src/App.js
@@ -1,10 +1,9 @@
+import React from "react";
+import { LoginPage } from './components/LoginPage';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-
-      </header>
-    </div>
+      <LoginPage/>
   );
 }
 
---
+++ react-exercise/src/components/LoginPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './LoginPage.css';
+
+export function LoginPage() {
+        return (
+            <div id="login-page-container">
+            </div>
+        );
+}
