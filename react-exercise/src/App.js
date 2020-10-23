85299198098faaa1c8d7b020de76612b5e01785e
Barnabás Mátrai <matrai.barnabas1@gmail.com>
2020-10-23 22:55:02 +0200

    add basic css to login page

--- react-exercise/src/App.js
+++ react-exercise/src/App.js
@@ -3,7 +3,10 @@ import { LoginPage } from './components/LoginPage';
 
 function App() {
   return (
-      <LoginPage/>
+      <div>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <LoginPage/>
+      </div>
   );
 }
 
