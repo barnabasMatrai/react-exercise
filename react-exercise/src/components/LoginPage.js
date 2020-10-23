08222c9e879ca89367587585cb0cba48906de78d
Barnabás Mátrai <matrai.barnabas1@gmail.com>
2020-10-23 22:48:34 +0200

    create basic login form

--- react-exercise/src/components/LoginPage.js
+++ react-exercise/src/components/LoginPage.js
@@ -4,6 +4,15 @@ import './LoginPage.css';
 export function LoginPage() {
         return (
             <div id="login-page-container">
+                <div id="login-container">
+                    <form id="login-form" method="post">
+                        <label htmlFor="email">Email address:</label>
+                        <input type="email" id="email" name="email"/>
+                        <label htmlFor="password">Password:</label>
+                        <input type="password" id="password" name="password"/>
+                        <input type="submit" value="Login"/>
+                    </form>
+                </div>
             </div>
         );
 }
