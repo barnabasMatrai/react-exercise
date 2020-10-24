be7b9912981943b3e9f56dd6e9c5631a3f6634b9
Barnabás Mátrai <matrai.barnabas1@gmail.com>
2020-10-24 09:10:27 +0200

    add /login route

--- react-exercise/src/App.js
+++ react-exercise/src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
 import { LoginPage } from './components/LoginPage';
+import { Switch, Route, BrowserRouter, Redirect } from "react-router-dom";
 
 function App() {
   return (
       <div>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-        <LoginPage/>
+          <BrowserRouter>
+              <Switch>
+                  <Route path="/login" component={LoginPage} />
+                  <Redirect from='/' to='/login' />
+              </Switch>
+          </BrowserRouter>
       </div>
   );
 }
