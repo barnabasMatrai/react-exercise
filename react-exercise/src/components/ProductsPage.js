8462ac541edbf7dd8f70cb1112b7f2487d8660c8
Barnabás Mátrai <matrai.barnabas1@gmail.com>
2020-10-24 14:20:44 +0200

    add some more items to product page

--- react-exercise/src/components/ProductsPage.js
+++ react-exercise/src/components/ProductsPage.js
@@ -7,7 +7,14 @@ export function ProductsPage() {
 
     const itemImages = { "https://bit.ly/2IYFyjb": "Hammer",
                         "https://bit.ly/31Gwpm2": "Box of nails",
-                        "https://bit.ly/2HtY16U": "Lock" };
+                        "https://bit.ly/2HtY16U": "Lock",
+                        "https://bit.ly/3ojhUyh": "Axe",
+                        "https://bit.ly/2J3whXd": "Drill",
+                        "https://bit.ly/3km71ct": "Measuring tape",
+                        "https://bit.ly/2Tka5tB": "Pliers",
+                        "https://bit.ly/3op1zrC": "Saw",
+                        "https://bit.ly/3juJmFs": "Screwdriver",
+                        "https://bit.ly/2IWUZbz": "Wrench"};
 
     for (let itemImagesKey in itemImages) {
         let image = React.createElement("img", { src: itemImagesKey, className: "item-image" });
